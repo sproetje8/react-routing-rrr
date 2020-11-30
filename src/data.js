126d17f6b43fad3506792e191b5c63c7c4550d99
Evelien Kulikova <evelienfoubert@mail.ru>
2020-11-30 21:56:08 +0300

    feat: Add data object for the data of the pages

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = {
+  reduce: {
+    'What?': 'Reduce-definition',
+    'How?': 'Reduce tips',
+    'More Information': ['resource1', 'resource2', 'resource3']
+  },
+  reuse: {
+    'What?': 'Reuse-definition',
+    'How?': 'Reuse tips',
+    'More Information': ['resource1', 'resource2', 'resource3']
+  },
+  recycle: {
+    'What?': 'Recycle-definition',
+    'How?': 'Recycle tips',
+    'More Information': ['resource1', 'resource2', 'resource3']
+  },  
+};
+
+export default data;
