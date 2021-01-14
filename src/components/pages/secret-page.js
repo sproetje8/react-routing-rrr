c66eaafc4688a5aade48b6fc694178cdf87efaca
Evelien Kulikova <evelienfoubert@mail.ru>
2021-01-14 17:55:46 +0300

    refact: Move React elements into constants and add ternary operator for rendering in SecretPage

--- src/components/pages/secret-page.js
+++ src/components/pages/secret-page.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 const SecretPage = ({isLoggedIn}) => {
-  if (isLoggedIn) {
-    return (
-      <div>
-        <h3>Here are lots of secrets!!!</h3>
-      </div>
-    );
-  }
+  const secrets = (
+    <div>
+      <h3>Here are lots of secrets!!!</h3>
+    </div>
+  );
 
-  return (<Redirect to='/login' />);
+  const redirect = (<Redirect to='/login' />);
+
+  return isLoggedIn ? secrets : redirect;
 };
 
 export default SecretPage;
