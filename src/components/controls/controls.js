68d4bcd2292c2021cf197b08bcad2cada8448e47
Evelien Kulikova <evelienfoubert@mail.ru>
2021-01-14 17:06:21 +0300

    fix: Replace data object with json object, create json bin and fetch data in Controls and Text components

--- src/components/controls/controls.js
+++ src/components/controls/controls.js
@@ -1,33 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import './controls.css';
-import data from '../../data';
+import ErrorIndicator from '../error-indicator';
 
-const Controls = ({ pageName, history }) => {
-  const controlHeaders = Object.keys(data[pageName]);
-  
-  const controlItems = controlHeaders.map((header) => {
-    const controlHeader = header.toLowerCase().replace('?', '').replace(' ', '-');
-        
-    return (
-      <li 
-        key={header}
-        className='controls__item'
-        onClick={() => {
-          history.push(controlHeader)
-        }}
-      >
-        {header}
-      </li>
-    );
+const dataURL = 'https://api.jsonbin.io/b/6000126d8aa7af359da9dbb5';
+
+class Controls extends Component {
+  state = {
+    isLoading: true,
+    hasError: false,
+    data: {}
+  }
+
+  componentDidMount = () => {
+    fetch(dataURL).then((res) => res.json()).then((data) => {
+      this.setState({
+        isLoading: false,
+        hasError: false,
+        data
+      });
+    })
+    .catch(() => {
+      this.setState({
+        isLoading: false,
+        hasError: true,
+        data: {}
+      });
     });
+  };
 
-  return (
-    <nav className='controls'>
-      <ul className='controls__list'>{controlItems}</ul>
-    </nav>
-  );
-};
+  render() {
+    const { isLoading, hasError, data } = this.state;
+    const { pageName, history } = this.props;
+    console.log(data);
+
+    if (isLoading) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+  
+    if (hasError) {
+      return <ErrorIndicator />;
+    }
+  
+    const controlHeaders = Object.keys(data.data[pageName]);
+    
+    const controlItems = controlHeaders.map((header) => {
+      const controlHeader = header.toLowerCase().replace('?', '').replace(' ', '-');
+          
+      return (
+        <li 
+          key={header}
+          className='controls__item'
+          onClick={() => {
+            history.push(controlHeader)
+          }}
+        >
+          {header}
+        </li>
+      );
+    });
+  
+    return (
+      <nav className='controls'>
+        <ul className='controls__list'>{controlItems}</ul>
+      </nav>
+    );
+  }
+}
 
 export default withRouter(Controls);
--- src/components/text/text.js
+++ src/components/text/text.js
@@ -1,44 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './text.css';
 
-import data from '../../data';
+import ErrorIndicator from '../error-indicator';
 
-const Text = ({ pageName, controlHeader }) => {
-  const pageData = data[pageName];
-  let dataKey = '';
+const dataURL = 'https://api.jsonbin.io/b/6000126d8aa7af359da9dbb5';
 
-  switch (controlHeader) {
-    case 'what':
-      dataKey = 'What?';
-      break;
-    case 'how':
-      dataKey = 'How?';
-      break;
-    case 'more-information':
-      dataKey = 'More Information';
-      break;
-    default:
-      break;
+class Text extends Component {
+
+  state = {
+    isLoading: true,
+    hasError: false,
+    data: {}
   }
 
-  const textData = pageData[dataKey];
-  let text;
-  
-  if (Array.isArray(textData)) {
-    const listItems = textData.map((item, index) => {
-        return <li key={index}>{item}</li>;
+  componentDidMount = () => {
+    fetch(dataURL).then((res) => res.json()).then((data) => {
+      this.setState({
+        isLoading: false,
+        hasError: false,
+        data
       });
-    text = <ul className='text__list'>{listItems}</ul>;
-  } else {
-    text = <p className='text__paragraph'>{textData}</p>;
-  }
+    })
+    .catch(() => {
+      this.setState({
+        isLoading: false,
+        hasError: true,
+        data: {}
+      });
+    });
+  };
 
-  return (
-    <div className='text'>
-        {text}
-    </div>
-  );
-};
+  render() {
+    const { isLoading, hasError, data } = this.state;
+    const { pageName, controlHeader } = this.props;
+
+    if (isLoading) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+  
+    if (hasError) {
+      return <ErrorIndicator />;
+    }
+
+    const pageData = data.data[pageName];
+    let dataKey = '';
+  
+    switch (controlHeader) {
+      case 'what':
+        dataKey = 'What?';
+        break;
+      case 'how':
+        dataKey = 'How?';
+        break;
+      case 'more-information':
+        dataKey = 'More Information';
+        break;
+      default:
+        break;
+    }
+
+    const textData = pageData[dataKey];
+    let text;
+    
+    if (Array.isArray(textData)) {
+      const listItems = textData.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        });
+      text = <ul className='text__list'>{listItems}</ul>;
+    } else {
+      text = <p className='text__paragraph'>{textData}</p>;
+    }
+
+    return (
+      <div className='text'>
+          {text}
+      </div>
+    );
+  }
+}
 
 export default Text;
