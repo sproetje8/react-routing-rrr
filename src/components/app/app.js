a8f5ca3331500784fb3d91fdfaea6fd7841d744c
Evelien Kulikova <evelienfoubert@mail.ru>
2021-01-22 14:24:13 +0300

    feat: Apply the useState Hook to App.js, changing class into functional component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './app.css';
 
@@ -14,46 +14,36 @@ import AppFooter from '../app-footer';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
-  state = {
-    isLoggedIn: false
-  }
-
-  onLogin = () => {
-    this.setState({ isLoggedIn: true });
-  };
-
-  render() {
-    const { isLoggedIn } = this.state;
-
-    return (
-      <Router>
-        <div className='app'>
-          <AppHeader />
-          <AppBody>
-            <Switch>
-              <Route path='/' exact component={HomePage} />
-              <Route path='/reduce/:id?' component={ReducePage} />
-              <Route path='/reuse/:id?' component={ReusePage} />
-              <Route path='/recycle/:id?' component={RecyclePage} />
-              <Route 
-                path='/login'
-                render={() => { 
-                  return <LoginPage 
-                            isLoggedIn={isLoggedIn}
-                            onLogin={() => {this.onLogin()}} />; }} 
-                        />
-              <Route 
-                path='/secret'
-                render={() => { return <SecretPage isLoggedIn={isLoggedIn} />; }} />
-              <Route render={() => <h2>Page not found!</h2>} />
-            </Switch>
-          </AppBody>
-          <AppFooter />
-        </div>
-      </Router>
-    );
-  }
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  return (
+    <Router>
+      <div className='app'>
+        <AppHeader />
+        <AppBody>
+          <Switch>
+            <Route path='/' exact component={HomePage} />
+            <Route path='/reduce/:id?' component={ReducePage} />
+            <Route path='/reuse/:id?' component={ReusePage} />
+            <Route path='/recycle/:id?' component={RecyclePage} />
+            <Route 
+              path='/login'
+              render={() => { 
+                return <LoginPage 
+                          isLoggedIn={isLoggedIn}
+                          onLogin={() => {setIsLoggedIn(true)}} />; }} 
+                      />
+            <Route 
+              path='/secret'
+              render={() => { return <SecretPage isLoggedIn={isLoggedIn} />; }} />
+            <Route render={() => <h2>Page not found!</h2>} />
+          </Switch>
+        </AppBody>
+        <AppFooter />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
